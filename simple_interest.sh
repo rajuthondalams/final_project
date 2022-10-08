5a0c74c39ac222b140a2cd6b05d2921524d01eaa
rajuthondalams <115253151+rajuthondalams@users.noreply.github.com>
2022-10-08 11:21:27 +0530

    chaniging
    read rate

--- simple_interest.sh
+++ simple_interest.sh
@@ -14,15 +14,15 @@
    # r, annual rate of interest
 
    # Output:
-   # simple interest = p*t*r
+   # simple interest = p*t*rate
 
    echo "Enter the principal:"
    read p
    echo "Enter rate of interest per year:"
-   read r
+   read rate
    echo "Enter time period in years:"
    read t
 
-   s=`expr $p \* $t \* $r / 100`
+   s=`expr $p \* $t \* $rate / 100`
    echo "The simple interest is: "
    echo $s
